23695ceaff086368ca9345016e423ef7fbde6469
alexandere2 <alexandere2@usf.edu>
2025-09-06 18:12:50 -0400

    Add files via upload
    Coding Challenge 2a: Product Pricing Assistant

---
+++ cc_2a.js
@@ -0,0 +1,20 @@
+
+const productName = "Jordan Sneakers";
+let costPerUnit = 150.00;
+let basePrice = 300.00;
+let discountRate = 0.25;
+let salesTaxRate = 0.10;
+let fixedMonthlyCosts = 1000
+
+let discountedPrice = basePrice * (1-discountRate);
+let finalPriceWithTax = discountedPrice * (1+salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts/profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+console.log("Jordan Sneakers");
+console.log ("Discounted Price: $" + discountedPrice);
+console.log ("Final Price with Tax: $" + finalPriceWithTax);
+console.log ("Profit Per Unit: $" + profitPerUnit);
+console.log ("Break-Even Units (Sneakers)" + breakEvenUnits);
+console.log (isProfitablePerUnit || 0);
